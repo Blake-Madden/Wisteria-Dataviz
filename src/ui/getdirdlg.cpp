786c1539fb00afd0ba41365c313f6562a0da3ef3
Blake Madden <madindayton@outlook.com>
2022-10-21 20:56:04 -0400

    Modernize/rename dir/file filter selection dialog

--- src/ui/getdirdlg.cpp
+++ src/ui/getdirdlg.cpp
@@ -6,16 +6,32 @@
 // SPDX-License-Identifier: BSD-3-Clause
 ///////////////////////////////////////////////////////////////////////////////
 
-
 #include "getdirdlg.h"
 
-wxBEGIN_EVENT_TABLE(wxGetDirDlg, DialogWithHelp)
-    EVT_BUTTON(wxGetDirDlg::ID_FOLDER_BROWSE_BUTTON, wxGetDirDlg::OnFolderButtonClick)
-    EVT_BUTTON(wxID_OK, wxGetDirDlg::OnOK)
-wxEND_EVENT_TABLE()
+using namespace Wisteria::UI;
 
 //-------------------------------------------------------------
-void wxGetDirDlg::OnOK([[maybe_unused]] wxCommandEvent& event)
+GetDirFilterDialog::GetDirFilterDialog(wxWindow* parent, const wxString& fullFileFilter,
+                    wxWindowID id /*= wxID_ANY*/,
+                    const wxString& caption /*= _("Select Directory")*/,
+                    const wxPoint& pos /*= wxDefaultPosition*/, const wxSize& size /*= wxDefaultSize*/,
+                    long style /*= wxDEFAULT_DIALOG_STYLE|wxCLIP_CHILDREN|wxRESIZE_BORDER*/) :
+                m_fullFileFilter(fullFileFilter)
+    {
+    SetExtraStyle(GetExtraStyle()|wxWS_EX_BLOCK_EVENTS|wxWS_EX_CONTEXTHELP);
+    DialogWithHelp::Create(parent, id, caption, pos, size, style);
+
+    CreateControls();
+    Centre();
+
+    // bind events
+    Bind(wxEVT_BUTTON, &GetDirFilterDialog::OnFolderButtonClick, this,
+         GetDirFilterDialog::ID_FOLDER_BROWSE_BUTTON);
+    Bind(wxEVT_BUTTON, &GetDirFilterDialog::OnOK, this, wxID_OK);
+    }
+
+//-------------------------------------------------------------
+void GetDirFilterDialog::OnOK([[maybe_unused]] wxCommandEvent& event)
     {
     TransferDataFromWindow();
 
@@ -33,7 +49,7 @@ void wxGetDirDlg::OnOK([[maybe_unused]] wxCommandEvent& event)
     }
 
 //-------------------------------------------------------------
-void wxGetDirDlg::OnFolderButtonClick([[maybe_unused]] wxCommandEvent& event)
+void GetDirFilterDialog::OnFolderButtonClick([[maybe_unused]] wxCommandEvent& event)
     {
     TransferDataFromWindow();
     wxDirDialog dirDlg(this);
@@ -46,40 +62,50 @@ void wxGetDirDlg::OnFolderButtonClick([[maybe_unused]] wxCommandEvent& event)
     }
 
 //-------------------------------------------------------------
-void wxGetDirDlg::CreateControls()
+void GetDirFilterDialog::CreateControls()
     {
     wxBoxSizer* mainSizer = new wxBoxSizer(wxVERTICAL);
 
     wxBoxSizer* fileBrowseBoxSizer = new wxBoxSizer(wxHORIZONTAL);
     mainSizer->Add(fileBrowseBoxSizer, 0, wxEXPAND|wxALL, wxSizerFlags::GetDefaultBorder());
 
-    wxTextCtrl* filePathEdit = new wxTextCtrl(this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxSize(FromDIP(wxSize(600,600)).GetWidth(),-1), wxTE_RICH2|wxBORDER_THEME, wxGenericValidator(&m_filePath) );
+    wxTextCtrl* filePathEdit = new wxTextCtrl(this, wxID_ANY, wxEmptyString, wxDefaultPosition,
+        wxSize(FromDIP(wxSize(600, 600)).GetWidth(), -1), wxTE_RICH2|wxBORDER_THEME,
+        wxGenericValidator(&m_filePath) );
     filePathEdit->AutoCompleteFileNames();
     fileBrowseBoxSizer->Add(filePathEdit, 1, wxEXPAND);
 
-    wxBitmapButton* fileBrowseButton = new wxBitmapButton(this, ID_FOLDER_BROWSE_BUTTON, wxArtProvider::GetBitmap(wxART_FILE_OPEN, wxART_BUTTON, FromDIP(wxSize(16,16))));
+    wxBitmapButton* fileBrowseButton = new wxBitmapButton(this, ID_FOLDER_BROWSE_BUTTON,
+        wxArtProvider::GetBitmap(wxART_FILE_OPEN, wxART_BUTTON, FromDIP(wxSize(16, 16))));
     fileBrowseBoxSizer->Add(fileBrowseButton, 0, wxALIGN_CENTER_VERTICAL);
 
     wxBoxSizer* fileTypeSizer = new wxBoxSizer(wxHORIZONTAL);
     mainSizer->Add(fileTypeSizer, 0, wxEXPAND);
-    fileTypeSizer->Add(new wxStaticText(this, wxID_STATIC, _("File types to include:"), wxDefaultPosition, wxDefaultSize, 0), 0, wxALIGN_CENTER_VERTICAL|wxLEFT, wxSizerFlags::GetDefaultBorder());
+    fileTypeSizer->Add(new wxStaticText(this, wxID_STATIC, _("File types to include:"),
+                                        wxDefaultPosition, wxDefaultSize, 0), 0,
+                                        wxALIGN_CENTER_VERTICAL|wxLEFT,
+                                        wxSizerFlags::GetDefaultBorder());
     wxArrayString choiceStrings;
-    wxStringTokenizer tkz(m_fullFileFilter, wxT("|"), wxTOKEN_STRTOK);
+    wxStringTokenizer tkz(m_fullFileFilter, L"|", wxTOKEN_STRTOK);
     while (tkz.HasMoreTokens() )
         {
         wxString currentFilter = tkz.GetNextToken();
-        if (currentFilter.length() && currentFilter[0] != wxT('*'))
+        if (currentFilter.length() && currentFilter[0] != L'*')
             { choiceStrings.Add(currentFilter); }
         }
-    m_fileFilterCombo = new wxChoice(this, wxID_ANY, wxDefaultPosition, wxSize(FromDIP(wxSize(100,100)).GetWidth(),-1), choiceStrings,
-        0, wxGenericValidator(&m_selectedFileFilter));
+    m_fileFilterCombo = new wxChoice(this, wxID_ANY, wxDefaultPosition,
+                                     wxSize(FromDIP(wxSize(100, 100)).GetWidth(),-1),
+                                     choiceStrings, 0, wxGenericValidator(&m_selectedFileFilter));
     fileTypeSizer->Add(m_fileFilterCombo, 1, wxEXPAND|wxALL, wxSizerFlags::GetDefaultBorder());
 
-    wxCheckBox* recurseDirsCheckBox = new wxCheckBox(this, wxID_ANY, _("&Search directories recursively"), wxDefaultPosition, wxDefaultSize, 0, wxGenericValidator(&m_isRecursive) );
+    wxCheckBox* recurseDirsCheckBox = new wxCheckBox(this, wxID_ANY,
+        _("&Search directories recursively"), wxDefaultPosition, wxDefaultSize, 0,
+        wxGenericValidator(&m_isRecursive) );
     mainSizer->Add(recurseDirsCheckBox, 0, wxALIGN_LEFT|wxALL, wxSizerFlags::GetDefaultBorder());
     mainSizer->AddStretchSpacer(1);
 
-    mainSizer->Add(CreateSeparatedButtonSizer(wxOK|wxCANCEL|wxHELP), 0, wxEXPAND|wxALL, wxSizerFlags::GetDefaultBorder());
+    mainSizer->Add(CreateSeparatedButtonSizer(wxOK|wxCANCEL|wxHELP), 0,
+        wxEXPAND|wxALL, wxSizerFlags::GetDefaultBorder());
 
     SetSizerAndFit(mainSizer);
 
--- src/ui/getdirdlg.h
+++ src/ui/getdirdlg.h
@@ -24,74 +24,79 @@
 #include <wx/artprov.h>
 #include "dialogwithhelp.h"
 
-/** @brief Prompt for selecting a folder and a file filter for files to select from it.*/
-class wxGetDirDlg final : public Wisteria::UI::DialogWithHelp
+namespace Wisteria::UI
     {
-public:
-    /** @brief Constructor.
-        @param parent The parent window.
-        @param fullFileFilter The file filter.
-        @param id The window ID.
-        @param caption The title of the export dialog.
-        @param pos The screen position of the window.
-        @param size The window size.
-        @param style The window style (i.e., decorations and flags).*/
-    wxGetDirDlg(wxWindow* parent, const wxString& fullFileFilter, wxWindowID id = wxID_ANY, const wxString& caption = _("Select Directory"),
-                      const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxDefaultSize,
-                      long style = wxDEFAULT_DIALOG_STYLE|wxCLIP_CHILDREN|wxRESIZE_BORDER) :
-            m_fullFileFilter(fullFileFilter), m_isRecursive(true), m_selectedFileFilter(0), m_fileFilterCombo(nullptr)
+    /** @brief Prompt for selecting a folder,
+            a file filter for files to select from it,
+            and whether the search should be recursive.*/
+    class GetDirFilterDialog final : public DialogWithHelp
         {
-        SetExtraStyle(GetExtraStyle()|wxWS_EX_BLOCK_EVENTS|wxWS_EX_CONTEXTHELP);
-        Wisteria::UI::DialogWithHelp::Create(parent, id, caption, pos, size, style);
+    public:
+        /** @brief Constructor.
+            @param parent The parent window.
+            @param fullFileFilter The file filter.
+            @param id The window ID.
+            @param caption The title of the export dialog.
+            @param pos The screen position of the window.
+            @param size The window size.
+            @param style The window style (i.e., decorations and flags).*/
+        GetDirFilterDialog(wxWindow* parent, const wxString& fullFileFilter,
+            wxWindowID id = wxID_ANY,
+            const wxString& caption = _("Select Directory"),
+            const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxDefaultSize,
+            long style = wxDEFAULT_DIALOG_STYLE | wxCLIP_CHILDREN | wxRESIZE_BORDER);
+        /// @private
+        GetDirFilterDialog(const GetDirFilterDialog&) = delete;
+        /// @private
+        GetDirFilterDialog(GetDirFilterDialog&&) = delete;
+        /// @private
+        GetDirFilterDialog& operator=(const GetDirFilterDialog&) = delete;
+        /// @private
+        GetDirFilterDialog& operator=(GetDirFilterDialog&&) = delete;
 
-        CreateControls();
-        Centre();
-        }
+        /// @returns The path of the selected folder.
+        [[nodiscard]] const wxString& GetPath() const noexcept
+            { return m_filePath; }
+        /** @brief Sets the path of the default selected folder.
+            @param path The default folder path to select.*/
+        void SetPath(const wxString& path)
+            { m_filePath = path; }
 
-    /// @returns The path of the selected folder.
-    [[nodiscard]] const wxString& GetPath() const noexcept
-        { return m_filePath; }
-    /** Sets the path of the default selected folder.
-        @param path The default folder path to select.*/
-    void SetPath(const wxString& path)
-        { m_filePath = path; }
-    /** Sets the selected file filter.
-        @param filter The file filter to display.*/
-    void SetSelectedFileFilter(const wxString& filter)
-        {
-        const int pos = m_fileFilterCombo->FindString(filter);
-        if (pos != wxNOT_FOUND)
+        /** @brief Sets the selected file filter.
+            @param filter The file filter to display.*/
+        void SetSelectedFileFilter(const wxString& filter)
             {
-            m_selectedFileFilter = pos;
-            TransferDataToWindow();
+            const auto pos = m_fileFilterCombo->FindString(filter);
+            wxASSERT_MSG(pos != wxNOT_FOUND,
+                wxString::Format(L"Unable to find '%s' in file filter!", filter));
+            if (pos != wxNOT_FOUND)
+                {
+                m_selectedFileFilter = pos;
+                TransferDataToWindow();
+                }
             }
-        }
-    /// @returns The selected document filter.
-    [[nodiscard]] wxString GetSelectedFileFilter()
-        {
-        TransferDataFromWindow();
-        return m_fileFilterCombo->GetString(m_selectedFileFilter);
-        }
-    /// @returns True if "recursive" checkbox is checked.
-    [[nodiscard]] bool IsRecursive() const noexcept
-        { return m_isRecursive; }
-private:
-    wxGetDirDlg() noexcept : m_isRecursive(true), m_fileFilterCombo(nullptr), m_selectedFileFilter(0)
-      {}
-    void CreateControls();
-    void OnFolderButtonClick([[maybe_unused]] wxCommandEvent& event);
-    void OnOK([[maybe_unused]] wxCommandEvent& event);
+        /// @returns The selected file filter.
+        [[nodiscard]] wxString GetSelectedFileFilter()
+            {
+            TransferDataFromWindow();
+            return m_fileFilterCombo->GetString(m_selectedFileFilter);
+            }
+        /// @returns @c true if "recursive" checkbox is checked.
+        [[nodiscard]] bool IsRecursive() const noexcept
+            { return m_isRecursive; }
+    private:
+        void CreateControls();
+        void OnFolderButtonClick([[maybe_unused]] wxCommandEvent& event);
+        void OnOK([[maybe_unused]] wxCommandEvent& event);
 
-    static constexpr int ID_FOLDER_BROWSE_BUTTON = 10001;
-    wxString m_filePath;
-    wxString m_fullFileFilter;
-    int m_selectedFileFilter{ 0 };
-    bool m_isRecursive{ true };
-    wxChoice* m_fileFilterCombo{ nullptr };
-
-    wxDECLARE_NO_COPY_CLASS(wxGetDirDlg);
-    wxDECLARE_EVENT_TABLE();
-    };
+        static constexpr int ID_FOLDER_BROWSE_BUTTON = wxID_HIGHEST+1;
+        wxString m_filePath;
+        wxString m_fullFileFilter;
+        int m_selectedFileFilter{ 0 };
+        bool m_isRecursive{ true };
+        wxChoice* m_fileFilterCombo{ nullptr };
+        };
+    }
 
 /** @}*/
 
