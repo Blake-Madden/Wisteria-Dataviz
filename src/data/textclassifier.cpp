9d557cfa7cb45c58cee806758a10f776de39419a
Blake Madden <madindayton@outlook.com>
2022-11-22 21:55:28 -0500

    Add copyright header

--- src/data/textclassifier.cpp
+++ src/data/textclassifier.cpp
@@ -1,3 +1,11 @@
+///////////////////////////////////////////////////////////////////////////////
+// Name:        textclassifier.cpp
+// Author:      Blake Madden
+// Copyright:   (c) 2005-2022 Blake Madden
+// Licence:     3-Clause BSD licence
+// SPDX-License-Identifier: BSD-3-Clause
+///////////////////////////////////////////////////////////////////////////////
+
 #include "textclassifier.h"
 
 namespace Wisteria::Data
