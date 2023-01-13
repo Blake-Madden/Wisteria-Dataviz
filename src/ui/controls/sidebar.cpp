581677dc512e33523355eeee57c9ceffe96972b8
Blake Madden <madindayton@outlook.com>
2023-01-13 16:08:12 -0500

    Make refresh rect smaller when selecting subitems
    #22

--- src/ui/controls/sidebar.cpp
+++ src/ui/controls/sidebar.cpp
@@ -1005,20 +1005,33 @@ void SideBar::SelectSubItem(const size_t item, const size_t subItem,
         return;
         }
     const auto previouslySelectedFolder = m_selectedFolder.value_or(item);
+    const auto previouslySelectedSubItem = m_folders[previouslySelectedFolder].m_selectedItem ?
+        std::optional<size_t>(m_folders[previouslySelectedFolder].m_selectedItem.value()) :
+        std::nullopt;
+
     m_selectedFolder = item;
+    const auto needsExpanding = !m_folders[GetSelectedFolder().value()].m_isExpanded;
     m_folders[GetSelectedFolder().value()].Expand();
     m_folders[GetSelectedFolder().value()].m_selectedItem = subItem;
 
     EnsureFolderVisible(GetSelectedFolder().value());
     RecalcSizes();
 
+    // get the rects of the selected (prior and current) selected items
+    const auto previosuslySelectedRect = previouslySelectedSubItem ?
+        m_folders[previouslySelectedFolder].m_subItems[previouslySelectedSubItem.value()].m_Rect :
+        m_folders[previouslySelectedFolder].m_Rect;
+
+    const auto currentlySelectedRect =
+        m_folders[GetSelectedFolder().value()].m_subItems[subItem].m_Rect;
+
     int x{ 0 }, y{ 0 };
     CalcUnscrolledPosition(0, 0, &x, &y);
 
-    wxRect refreshRect{ GetClientRect() };
-    refreshRect.SetTop(std::min(
-        m_folders[previouslySelectedFolder].m_Rect.GetTop(),
-        m_folders[GetSelectedFolder().value()].m_Rect.GetTop()));
+    wxRect refreshRect =
+        (needsExpanding ?
+         GetClientRect() :
+         previosuslySelectedRect.Union(currentlySelectedRect));
     refreshRect.Offset(0, -std::min(y, refreshRect.y));
     
     Refresh(true, &refreshRect);
