f1f3e7124d319448d809a951dc7f761c3b39e509
Blake Madden <madindayton@outlook.com>
2023-02-05 13:52:36 -0500

    Add RFT clipboard helper to project

---
+++ src/util/clipboard_rtf.h
@@ -0,0 +1,65 @@
+#ifndef __WX_CLIPBOARD_RTF_H__
+#define __WX_CLIPBOARD_RTF_H__
+
+#include <wx/dataobj.h>
+#include <wx/string.h>
+#include <memory>
+
+/** @brief A specialization of @c wxDataObjectSimple for Rich Text Formatted text.
+    @details It can be used without change to paste data into the wxClipboard
+        or a @c wxDropSource.*/
+class wxRtfDataObject : public wxDataObjectSimple
+    {
+public:
+    /** @brief Constructor. May be used to initialise the text
+            (otherwise, SetText() should be used later).
+        @param rtf The RTF data.*/
+    wxRtfDataObject(const wxString& rtf = wxEmptyString)
+         :
+    #ifdef __WXMSW__
+        wxDataObjectSimple(L"Rich Text Format"),
+    #elif defined __WXOSX__
+        wxDataObjectSimple(L"public.rtf"),
+    #else
+        wxDataObjectSimple(L"text/rtf"),
+    #endif
+           m_rtf(rtf)
+        {}
+    /** @brief Sets the (Rich Text Formatted) text.
+        @param rtf The RTF data.*/
+    void SetText(const wxString& rtf)
+        { m_rtf = rtf; }
+    /** @param format The data's format (not used).
+        @returns The size of the RTF data in bytes. */
+    [[nodiscard]]
+    size_t GetDataSize([[maybe_unused]] const wxDataFormat& format) const final
+        { return GetDataSize(); }
+    /** @returns The size of the RTF data in bytes. */
+    [[nodiscard]]
+    size_t GetDataSize() const final
+        {
+        const wxCharBuffer cBuffer = m_rtf.mb_str();
+        return std::strlen(cBuffer.data());
+        }
+    /** @brief Copy the data to the buffer.
+        @param format The data's format (not used).
+        @param[out] buf The data buffer to write to.
+        @returns @c true on success. */
+    [[nodiscard]]
+    bool GetDataHere([[maybe_unused]] const wxDataFormat& format, void *buf) const final
+        { return GetDataHere(buf); }
+    /** @brief Copy the data to the buffer.
+        @param[out] buf The data buffer to write to.
+        @returns @true on success.*/
+    [[nodiscard]]
+    bool GetDataHere(void *buf) const final
+        {
+        const wxCharBuffer cBuffer = m_rtf.mb_str();
+        std::memcpy(buf, cBuffer.data(), std::strlen(cBuffer.data()));
+        return true;
+        }
+private:
+    wxString m_rtf;
+    };
+
+#endif //__WX_CLIPBOARD_RTF_H__
