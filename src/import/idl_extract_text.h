6b27f57d9d24b1ae43261cff8ecd5cb596586682
Blake Madden <madindayton@outlook.com>
2023-04-23 10:05:18 -0400

    Update copyright

--- src/import/idl_extract_text.h
+++ src/import/idl_extract_text.h
@@ -1,6 +1,6 @@
 /** @addtogroup Importing
     @brief Classes for importing and parsing text.
-    @date 2005-2020
+    @date 2005-2023
     @copyright Oleander Software, Ltd.
     @author Blake Madden
     @details This program is free software; you can redistribute it and/or modify
