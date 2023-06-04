0cc0f853e19369030695bfa46bc08e87d45f9dc4
Blake Madden <madindayton@outlook.com>
2023-06-04 09:32:37 -0400

    Add project for PREFast code analysis

---
+++ tests/PREFast/wdvprefast.cpp
@@ -0,0 +1,35 @@
+// This is mostly used as a CMake project used for code analysis.
+
+#include "src/import/cpp_extract_text.h"
+#include "src/import/doc_extract_text.h"
+#include "src/import/docx_extract_text.h"
+#include "src/import/extract_text.h"
+#include "src/import/hhc_hhk_extract_text.h"
+#include "src/import/html_encode.h"
+#include "src/import/html_extract_text.h"
+#include "src/import/idl_extract_text.h"
+#include "src/import/markdown_extract_text.h"
+#include "src/import/odt_odp_extract_text.h"
+#include "src/import/postscript_extract_text.h"
+#include "src/import/pptx_extract_text.h"
+#include "src/import/rtf_encode.h"
+#include "src/import/rtf_extract_text.h"
+#include "src/import/tag_filter.h"
+#include "src/import/text_colum"
+#include "src/import/text_functional.h"
+#include "src/import/text_matrix.h"
+#include "src/import/text_preview.h"
+#include "src/import/text_row.h"
+#include "src/import/unicode_extract_text.h"
+#include "src/import/xlsx_extract_text.h"
+#include "src/math/mathematics.h"
+#include "src/math/safe_math.h"
+#include "src/math/statistics.h"
+#include "src/util/backupvariable.h"
+#include "src/util/frequencymap.h"
+#include "src/util/numberformat.h"
+
+int main()
+    {
+    return EXIT_SUCCESS;
+    }
