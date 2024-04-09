ae6c194928d02c50f04c35c9676e920bc261bfbf
Blake-Madden <madindayton@outlook.com>
2024-04-09 05:51:32 -0400

    clang-format

--- src/base/shapes.h
+++ src/base/shapes.h
@@ -12,9 +12,9 @@
 #ifndef __WISTERIA_SHAPES_H__
 #define __WISTERIA_SHAPES_H__
 
-#include "graphitems.h"
-#include "colorbrewer.h"
 #include "changers.h"
+#include "colorbrewer.h"
+#include "graphitems.h"
 #include <random>
 
 namespace Wisteria::GraphItems
@@ -30,7 +30,7 @@ namespace Wisteria::GraphItems
             upon destruction of this object.*/
     class GraphicsContextFallback
         {
-    public:
+      public:
         /** @brief Takes a @c wxDC and a rectangle area within it, and creates a
                 @c wxGraphicsContext to render to that area.
             @param dc A pointer to the original @c wxDC that is being drawn to.
@@ -42,6 +42,7 @@ namespace Wisteria::GraphItems
         GraphicsContextFallback(const GraphicsContextFallback&) = delete;
         /// @private
         GraphicsContextFallback& operator=(const GraphicsContextFallback&) = delete;
+
         /** @returns The @c wxGraphicsContext to render to.\n
                 (May be null if a failure occurred).
             @note The returned @c wxGraphicsContext may point to the original @c wxDC, or it may
@@ -49,16 +50,20 @@ namespace Wisteria::GraphItems
                 this object goes out of scope. Either way, just use the returned
                 @c wxGraphicsContext to draw to and the rendering/blitting will be applied when
                 this object goes out of scope.*/
-        wxGraphicsContext* GetGraphicsContext() noexcept
-            { return m_gc; }
+        wxGraphicsContext* GetGraphicsContext() noexcept { return m_gc; }
+
         /// @private
         ~GraphicsContextFallback();
+
         /// @returns @c true if an advanced graphics context could not be acquired
         ///     and rendering will fall back to drawing to a bitmap.
         [[nodiscard]]
         bool IsFallingBackToBitmap() const noexcept
-            { return m_drawingToBitmap; }
-    private:
+            {
+            return m_drawingToBitmap;
+            }
+
+      private:
         wxGraphicsContext* m_gc{ nullptr };
         wxDC* m_dc{ nullptr };
         wxMemoryDC m_memDC;
@@ -78,7 +83,7 @@ namespace Wisteria::GraphItems
             and Shape or FillableShape for drawing on a Canvas.*/
     class ShapeRenderer
         {
-    public:
+      public:
         /// @private
         using DrawFunction = void (ShapeRenderer::*)(const wxRect rect, wxDC& dc) const;
 
@@ -89,24 +94,30 @@ namespace Wisteria::GraphItems
             @param itemInfo Extended information (e.g., brush, pen) to draw with.\n
                 Which features are used are shape dependent.
             @param img An image to use for the shape if using IconShape::ImageIcon.*/
-        explicit ShapeRenderer(const GraphItemInfo& itemInfo,
-                               const wxBitmapBundle* img = nullptr) :
-            m_graphInfo(itemInfo),
-            m_iconImage(img)
-            {}
+        explicit ShapeRenderer(const GraphItemInfo& itemInfo, const wxBitmapBundle* img = nullptr)
+            : m_graphInfo(itemInfo), m_iconImage(img)
+            {
+            }
+
         /// @private
         ShapeRenderer() = delete;
+
         /// @brief Gets/sets the shape's underlying information (e.g., brush color, pen, etc.).
         /// @details This is useful for changing the shape's settings when preparing to
         ///     draw different shapes.
         /// @returns The shape's information.
         [[nodiscard]]
         GraphItemInfo& GetGraphItemInfo() noexcept
-            { return m_graphInfo; }
+            {
+            return m_graphInfo;
+            }
+
         /// @private
         [[nodiscard]]
         const GraphItemInfo& GetGraphItemInfo() const noexcept
-            { return m_graphInfo; }
+            {
+            return m_graphInfo;
+            }
 
         /// @name Shape Rendering Functions
         /// @brief Functions for drawing shapes within a provided rectangle.
@@ -118,8 +129,8 @@ namespace Wisteria::GraphItems
         /// @param brush The brush for the sign's background.
         /// @param text The text to draw on the sign.
         /// @param dc The DC to draw to.
-        void DrawCircularSign(const wxRect rect, const wxBrush& brush,
-                              const wxString& text, wxDC& dc) const;
+        void DrawCircularSign(const wxRect rect, const wxBrush& brush, const wxString& text,
+                              wxDC& dc) const;
         /// @brief Draws a yellow sun shape (circle with sunbeams).
         /// @param rect The area to draw the sun within.
         /// @param dc The DC to draw to.
@@ -141,26 +152,39 @@ namespace Wisteria::GraphItems
         ///     where @c Top will draw curly braces that are opening downward
         ///     (enclosing what is beneath it).
         void DrawCurlyBrace(const wxRect rect, wxDC& dc, const Side side) const;
+
         /// @brief Draws a left curly brace.
         /// @param rect The area to draw the curly brace within.
         /// @param dc The DC to draw to.
         void DrawLeftCurlyBrace(const wxRect rect, wxDC& dc) const
-            { DrawCurlyBrace(rect, dc, Side::Left); }
+            {
+            DrawCurlyBrace(rect, dc, Side::Left);
+            }
+
         /// @brief Draws a right curly brace.
         /// @param rect The area to draw the curly brace within.
         /// @param dc The DC to draw to.
         void DrawRightCurlyBrace(const wxRect rect, wxDC& dc) const
-            { DrawCurlyBrace(rect, dc, Side::Right); }
+            {
+            DrawCurlyBrace(rect, dc, Side::Right);
+            }
+
         /// @brief Draws a top curly brace.
         /// @param rect The area to draw the curly brace within.
         /// @param dc The DC to draw to.
         void DrawTopCurlyBrace(const wxRect rect, wxDC& dc) const
-            { DrawCurlyBrace(rect, dc, Side::Top); }
+            {
+            DrawCurlyBrace(rect, dc, Side::Top);
+            }
+
         /// @brief Draws a bottom curly brace.
         /// @param rect The area to draw the curly brace within.
         /// @param dc The DC to draw to.
         void DrawBottomCurlyBrace(const wxRect rect, wxDC& dc) const
-            { DrawCurlyBrace(rect, dc, Side::Bottom); }
+            {
+            DrawCurlyBrace(rect, dc, Side::Bottom);
+            }
+
         /// @brief Draws a square.
         /// @param rect The area to draw the square within.
         /// @param dc The DC to draw to.
@@ -316,63 +340,82 @@ namespace Wisteria::GraphItems
         /// @param dc The DC to draw to.
         void DrawRuler(const wxRect rect, wxDC& dc) const;
         /// @}
-    private:
+      private:
         void DrawAsterisk(wxRect rect, wxGraphicsContext* gc) const;
         void DrawTire(wxRect rect, wxGraphicsContext* gc) const;
         /// @brief Sets the base color (if in use), performs the provided rendering lambda,
         ///     sets the brush, then runs the rendering lambda again.
         void DrawWithBaseColorAndBrush(wxDC& dc, const std::function<void(void)>& fn) const;
+
         /// @brief Offsets calls to GetXPosFromLeft(). This is useful for centering the rendering
         ///     of irregular (i.e., non-square) shapes inside of squares.
         void SetXOffsetPercentage(const double offset) const noexcept
-            { m_xOffsetPercentage = offset; }
+            {
+            m_xOffsetPercentage = offset;
+            }
+
         /// @brief Offsets calls to GetYPosFromLeft(). This is useful for centering the rendering
         ///     of irregular (i.e., non-square) shapes inside of squares.
         void SetYOffsetPercentage(const double offset) const noexcept
-            { m_yOffsetPercentage = offset; }
+            {
+            m_yOffsetPercentage = offset;
+            }
+
         /// @brief Helper to get X coordinate based on percent of width of rect from its left side.
         /// @note @c percentFromLeft can be negative if using it for Bezier control points
         ///     that need to go a little outside of the rect.
         [[nodiscard]]
-        double GetXPosFromLeft(const wxRect rect,
-                               const double percentFromLeft) const
-            { return rect.GetLeft() + (rect.GetWidth() * (percentFromLeft + m_xOffsetPercentage)); };
+        double GetXPosFromLeft(const wxRect rect, const double percentFromLeft) const
+            {
+            return rect.GetLeft() + (rect.GetWidth() * (percentFromLeft + m_xOffsetPercentage));
+            }
+
         /// @brief Helper to get Y coordinate based on percent of height of rect from its top.
         [[nodiscard]]
-        double GetYPosFromTop(const wxRect rect,
-                              const double percentFromTop) const
-            { return rect.GetTop() + (rect.GetHeight() * (percentFromTop + m_yOffsetPercentage)); };
+        double GetYPosFromTop(const wxRect rect, const double percentFromTop) const
+            {
+            return rect.GetTop() + (rect.GetHeight() * (percentFromTop + m_yOffsetPercentage));
+            }
 
         /// @brief Mirrors percentages passed to GetXPosFromLeft() or GetYPosFromTop().
         [[nodiscard]]
         constexpr double Mirror(const double percent) const noexcept
-            { return 1.0 - percent; }
+            {
+            return 1.0 - percent;
+            }
 
         /// @returns The midpoint of a rect.
         [[nodiscard]]
         wxPoint GetMidPoint(const wxRect rect) const
-            { return rect.GetLeftTop() + wxPoint(rect.GetWidth()/2, rect.GetHeight()/2); }
+            {
+            return rect.GetLeftTop() + wxPoint(rect.GetWidth() / 2, rect.GetHeight() / 2);
+            }
 
         /// @returns The radius of the largest circle that can fit in a rect.
         /// @note This is floored to be conservative.
         [[nodiscard]]
         double GetRadius(const wxRect rect) const
             {
-            return std::floor(
-                safe_divide<double>(std::min(rect.GetWidth(), rect.GetHeight()), 2));
+            return std::floor(safe_divide<double>(std::min(rect.GetWidth(), rect.GetHeight()), 2));
             }
 
         [[nodiscard]]
         double ScaleToScreenAndCanvas(const double value) const noexcept
-            { return value * GetScaling() * GetDPIScaleFactor(); }
+            {
+            return value * GetScaling() * GetDPIScaleFactor();
+            }
 
         [[nodiscard]]
         double DownscaleFromScreenAndCanvas(const double value) const noexcept
-            { return safe_divide(value, (GetScaling() * GetDPIScaleFactor())); }
+            {
+            return safe_divide(value, (GetScaling() * GetDPIScaleFactor()));
+            }
 
         [[nodiscard]]
         double GetScaling() const noexcept
-            { return m_graphInfo.GetScaling(); }
+            {
+            return m_graphInfo.GetScaling();
+            }
 
         [[nodiscard]]
         double GetDPIScaleFactor() const noexcept
@@ -381,6 +424,7 @@ namespace Wisteria::GraphItems
                    L"Shape should have a proper DPI scaling.");
             return m_graphInfo.GetDPIScaleFactor().value_or(1);
             }
+
         /// @returns A color with the shape's brush opacity applied to it.
         /// @param col The color to adjust.
         /// @note This should be performed on any custom brush (but *not* outline pen) colors
@@ -390,8 +434,8 @@ namespace Wisteria::GraphItems
         [[nodiscard]]
         wxColour ApplyParentColorOpacity(const wxColour col) const
             {
-            return Colors::ColorContrast::ChangeOpacity(col,
-                GetGraphItemInfo().GetBrush().GetColour().GetAlpha());
+            return Colors::ColorContrast::ChangeOpacity(
+                col, GetGraphItemInfo().GetBrush().GetColour().GetAlpha());
             }
 
         GraphItemInfo m_graphInfo;
@@ -404,16 +448,14 @@ namespace Wisteria::GraphItems
     /** @brief Draws a shape onto a canvas.*/
     class Shape : public GraphItemBase
         {
-    public:
-         /** @brief Constructor.
-             @param itemInfo Base information for the shape.
-             @param shape The icon shape to draw.
-             @param sz The size of the shape (in DIPs).
-             @param img An image to use for the shape if using IconShape::ImageIcon.*/
-        Shape(const GraphItems::GraphItemInfo& itemInfo,
-              const Icons::IconShape shape,
-              const wxSize sz,
-              const wxBitmapBundle* img = nullptr);
+      public:
+        /** @brief Constructor.
+            @param itemInfo Base information for the shape.
+            @param shape The icon shape to draw.
+            @param sz The size of the shape (in DIPs).
+            @param img An image to use for the shape if using IconShape::ImageIcon.*/
+        Shape(const GraphItems::GraphItemInfo& itemInfo, const Icons::IconShape shape,
+              const wxSize sz, const wxBitmapBundle* img = nullptr);
         /// @private
         Shape(const Shape&) = delete;
         /// @private
@@ -433,62 +475,79 @@ namespace Wisteria::GraphItems
             @param drawRect The rect to draw within.
             @param dc The DC to render onto.*/
         void Draw(const wxRect& drawRect, wxDC& dc) const;
+
         /// @returns What type of shape is being drawn.
-        Icons::IconShape GetShape() const noexcept
-            { return m_shape; }
+        Icons::IconShape GetShape() const noexcept { return m_shape; }
+
         /// @returns The size in DIPs.
         [[nodiscard]]
         wxSize GetSizeDIPS() const noexcept
-            { return m_sizeDIPs; }
-    protected:
+            {
+            return m_sizeDIPs;
+            }
+
+      protected:
         /// @returns The rectangle on the canvas where the shape would fit in.
         /// @param dc Measurement DC, which is not used in this implementation.
         [[nodiscard]]
         wxRect GetBoundingBox([[maybe_unused]] wxDC& dc) const final;
+
         /** @brief Moves the shape by the specified x and y values.
             @param xToMove the amount to move horizontally.
             @param yToMove the amount to move vertically.*/
         void Offset(const int xToMove, const int yToMove) noexcept final
-            { SetAnchorPoint(GetAnchorPoint() + wxPoint(xToMove, yToMove)); }
+            {
+            SetAnchorPoint(GetAnchorPoint() + wxPoint(xToMove, yToMove));
+            }
+
         /// @returns The renderer.
         [[nodiscard]]
         ShapeRenderer& GetRenderer() noexcept
-            { return m_renderer; }
-    protected:
+            {
+            return m_renderer;
+            }
+
+      protected:
         /// @private
         wxSize m_shapeSizeDIPs{ 0, 0 };
         /// @private
         wxSize m_sizeDIPs{ 0, 0 };
-    private:
+
+      private:
         [[nodiscard]]
         GraphItemInfo& GetGraphItemInfo() final
             {
             m_rendererNeedsUpdating = true;
             return GraphItemBase::GetGraphItemInfo();
             }
+
         void SetDPIScaleFactor(const double scaling) final
             {
             GraphItemBase::SetDPIScaleFactor(scaling);
             m_renderer.m_graphInfo.DPIScaling(scaling);
             }
+
         void SetScaling(const double scaling) final
             {
             GraphItemBase::SetScaling(scaling);
             m_renderer.m_graphInfo.Scaling(scaling);
             }
+
         /** @returns @c true if the given point is inside of the shape.
             @param pt The point to check.*/
         [[nodiscard]]
         bool HitTest(const wxPoint pt, wxDC& dc) const noexcept final
-            { return GetBoundingBox(dc).Contains(pt); }
+            {
+            return GetBoundingBox(dc).Contains(pt);
+            }
 
         Icons::IconShape m_shape{ Icons::IconShape::Square };
         mutable ShapeRenderer m_renderer;
         mutable bool m_rendererNeedsUpdating{ true };
         ShapeRenderer::DrawFunction m_drawFunction{ nullptr };
         };
-    };
+    }; // namespace Wisteria::GraphItems
 
-/** @}*/
+    /** @}*/
 
 #endif //__WISTERIA_SHAPES_H__
