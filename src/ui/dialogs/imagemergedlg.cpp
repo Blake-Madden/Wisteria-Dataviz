d8c2f9140736972146d28d7e57c6f16b0318c214
Blake-Madden <madindayton@outlook.com>
2024-05-03 12:40:30 -0400

    Add dialog for merging images Uses the various image stitching functions.

---
+++ src/ui/dialogs/imagemergedlg.cpp
@@ -0,0 +1,204 @@
+///////////////////////////////////////////////////////////////////////////////
+// Name:        imagemergedlg.cpp
+// Author:      Blake Madden
+// Copyright:   (c) 2005-2023 Blake Madden
+// License:     3-Clause BSD license
+// SPDX-License-Identifier: BSD-3-Clause
+///////////////////////////////////////////////////////////////////////////////
+
+#include "imagemergedlg.h"
+
+//----------------------------------------
+void ImageMergeDlg::CreateControls(const wxArrayString& imgPaths)
+    {
+    wxBoxSizer* mainSizer = new wxBoxSizer(wxVERTICAL);
+
+    wxArrayString orientations = { _(L"Horizontal"), _(L"Vertical") };
+    wxRadioBox* orientationsRadioBox =
+        new wxRadioBox(this, wxID_ANY, _(L"Images' Layout"), wxDefaultPosition, wxDefaultSize,
+                       orientations, 0, wxRA_SPECIFY_ROWS, wxGenericValidator(&m_orientRadio));
+    orientationsRadioBox->SetSelection(0);
+    mainSizer->Add(orientationsRadioBox, wxSizerFlags().Border(
+                                             wxDirection::wxALL, wxSizerFlags::GetDefaultBorder()));
+
+    m_horizontalThumbsSizer = new wxStaticBoxSizer(wxHORIZONTAL, this);
+
+    for (const auto& imgPath : imgPaths)
+        {
+        m_horizontalThumbsSizer->Add(new Wisteria::UI::Thumbnail(
+            m_horizontalThumbsSizer->GetStaticBox(), Wisteria::GraphItems::Image::LoadFile(imgPath),
+            Wisteria::ClickMode::BrowseForImageFile, true));
+        }
+
+    AdjustThumbnailsHorizontally();
+
+    mainSizer->Add(
+        m_horizontalThumbsSizer,
+        wxSizerFlags(1).Expand().Border(wxDirection::wxALL, wxSizerFlags::GetDefaultBorder()));
+
+    m_verticalThumbsSizer = new wxStaticBoxSizer(wxVERTICAL, this);
+
+    for (const auto& imgPath : imgPaths)
+        {
+        m_verticalThumbsSizer->Add(new Wisteria::UI::Thumbnail(
+            m_verticalThumbsSizer->GetStaticBox(), Wisteria::GraphItems::Image::LoadFile(imgPath),
+            Wisteria::ClickMode::BrowseForImageFile, true));
+        }
+
+    AdjustThumbnailsVertically();
+
+    mainSizer->Add(
+        m_verticalThumbsSizer,
+        wxSizerFlags(1).Expand().Border(wxDirection::wxALL, wxSizerFlags::GetDefaultBorder()));
+
+    mainSizer->Add(
+        new wxStaticText(this, wxID_STATIC,
+                         _(L"Click any thumbnail to select a different image.\n"
+                           "Click OK to combine images into a new one.")),
+        wxSizerFlags().Expand().Border(wxDirection::wxALL, wxSizerFlags::GetDefaultBorder()));
+
+    mainSizer->Add(
+        CreateSeparatedButtonSizer(wxOK | wxCANCEL),
+        wxSizerFlags().Expand().Border(wxDirection::wxALL, wxSizerFlags::GetDefaultBorder()));
+
+    m_horizontalThumbsSizer->Show(m_orientRadio == 0);
+    m_verticalThumbsSizer->Show(m_orientRadio == 1);
+
+    SetSizerAndFit(mainSizer);
+
+    Bind(wxEVT_RADIOBOX,
+         [this]([[maybe_unused]] wxCommandEvent)
+         {
+             TransferDataFromWindow();
+             m_horizontalThumbsSizer->Show(m_orientRadio == 0);
+             m_verticalThumbsSizer->Show(m_orientRadio == 1);
+             if (m_orientRadio == 0)
+                 {
+                 AdjustThumbnailsHorizontally();
+                 }
+             else
+                 {
+                 AdjustThumbnailsVertically();
+                 }
+         });
+    Bind(wxEVT_THUMBNAIL_CHANGED,
+         [this]([[maybe_unused]] wxCommandEvent)
+         {
+             if (m_orientRadio == 0)
+                 {
+                 AdjustThumbnailsHorizontally();
+                 }
+             else
+                 {
+                 AdjustThumbnailsVertically();
+                 }
+         });
+    Bind(
+        wxEVT_BUTTON,
+        [this]([[maybe_unused]] wxCommandEvent)
+        {
+            wxFileDialog fd(this, _(L"Select Output Image"), wxString{}, wxString{},
+                            Wisteria::GraphItems::Image::GetImageFileFilter(),
+                            wxFD_OPEN | wxFD_PREVIEW);
+            if (fd.ShowModal() != wxID_OK)
+                {
+                return;
+                }
+
+            std::vector<wxImage> images;
+            for (const auto* sizerItem : m_horizontalThumbsSizer->GetChildren())
+                {
+                if (const auto* thumb{
+                        dynamic_cast<const Wisteria::UI::Thumbnail*>(sizerItem->GetWindow()) };
+                    thumb != nullptr)
+                    {
+                    images.push_back(thumb->GetImage().GetOriginalImage());
+                    }
+                }
+
+            m_mergedFilePath = fd.GetPath();
+
+            if (m_orientRadio == 0)
+                {
+                Wisteria::GraphItems::Image::StitchHorizontally(images).SaveFile(m_mergedFilePath);
+                }
+            else
+                {
+                Wisteria::GraphItems::Image::StitchVertically(images).SaveFile(m_mergedFilePath);
+                }
+
+            if (IsModal())
+                {
+                EndModal(wxID_OK);
+                }
+            else
+                {
+                Show(false);
+                }
+        },
+        wxID_OK);
+    }
+
+//----------------------------------------
+void ImageMergeDlg::AdjustThumbnailsHorizontally()
+    {
+    int maxHeight{ 0 };
+    for (const auto* sizerItem : m_horizontalThumbsSizer->GetChildren())
+        {
+        if (const auto* thumb{
+                dynamic_cast<const Wisteria::UI::Thumbnail*>(sizerItem->GetWindow()) };
+            thumb != nullptr)
+            {
+            maxHeight = std::max(thumb->GetImage().GetOriginalImage().GetHeight(), maxHeight);
+            }
+        }
+    for (auto* sizerItem : m_horizontalThumbsSizer->GetChildren())
+        {
+        if (auto* thumb{ dynamic_cast<Wisteria::UI::Thumbnail*>(sizerItem->GetWindow()) };
+            thumb != nullptr)
+            {
+            const double percentOfMaxHeight{ safe_divide<double>(
+                thumb->GetImage().GetOriginalImage().GetHeight(), maxHeight) };
+            wxSize bestSize = thumb->GetImage().GetBestSize(
+                wxSize{ FromDIP(512), static_cast<int>(percentOfMaxHeight * FromDIP(512)) });
+            sizerItem->SetMinSize(bestSize);
+            }
+        }
+
+    if (GetSizer() != nullptr)
+        {
+        GetSizer()->Fit(this);
+        }
+    }
+
+//----------------------------------------
+void ImageMergeDlg::AdjustThumbnailsVertically()
+    {
+    int maxWidth{ 0 };
+    for (const auto* sizerItem : m_verticalThumbsSizer->GetChildren())
+        {
+        if (const auto* thumb{
+                dynamic_cast<const Wisteria::UI::Thumbnail*>(sizerItem->GetWindow()) };
+            thumb != nullptr)
+            {
+            maxWidth = std::max(thumb->GetImage().GetOriginalImage().GetWidth(), maxWidth);
+            }
+        }
+    for (auto* sizerItem : m_verticalThumbsSizer->GetChildren())
+        {
+        if (auto* thumb{ dynamic_cast<Wisteria::UI::Thumbnail*>(sizerItem->GetWindow()) };
+            thumb != nullptr)
+            {
+            const double percentOfMaxWidth{ safe_divide<double>(
+                thumb->GetImage().GetOriginalImage().GetWidth(), maxWidth) };
+            wxSize bestSize = thumb->GetImage().GetBestSize(
+                wxSize{ static_cast<int>(percentOfMaxWidth * FromDIP(512)), FromDIP(512) });
+            sizerItem->SetMinSize(bestSize);
+            }
+        }
+
+    if (GetSizer() != nullptr)
+        {
+        GetSizer()->Fit(this);
+        }
+    }
---
+++ src/ui/dialogs/imagemergedlg.h
@@ -0,0 +1,82 @@
+/** @addtogroup UI
+    @brief User interface classes.
+    @date 2005-2023
+    @copyright Blake Madden
+    @author Blake Madden
+    @details This program is free software; you can redistribute it and/or modify
+     it under the terms of the 3-Clause BSD License.
+
+     SPDX-License-Identifier: BSD-3-Clause
+@{*/
+
+#ifndef __IMAGEMERGE_DLG_H__
+#define __IMAGEMERGE_DLG_H__
+
+#include "../controls/listctrlex.h"
+#include "../controls/thumbnail.h"
+#include "dialogwithhelp.h"
+#include <vector>
+#include <wx/filename.h>
+#include <wx/infobar.h>
+#include <wx/radiobox.h>
+#include <wx/richmsgdlg.h>
+#include <wx/sizer.h>
+#include <wx/string.h>
+#include <wx/wx.h>
+
+/** @brief Dialog which displays images that can be merged into another one
+        (vertically or horizontally).*/
+class ImageMergeDlg final : public Wisteria::UI::DialogWithHelp
+    {
+  public:
+    /** @brief Constructor.
+        @param parent The parent window.
+        @param imgPaths The images to merge.
+        @param orientation Whether to stitch the images horizontally or vertically.
+        @param id The window ID.
+        @param caption The title of the dialog.
+        @param pos The screen position of the window.
+        @param size The window size.
+        @param style The window style (i.e., decorations and flags).*/
+    explicit ImageMergeDlg(wxWindow* parent, const wxArrayString& imgPaths,
+                           wxOrientation orientation, wxWindowID id = wxID_ANY,
+                           const wxString& caption = _(L"Merge Images"),
+                           const wxPoint& pos = wxDefaultPosition,
+                           const wxSize& size = wxDefaultSize,
+                           long style = wxDEFAULT_DIALOG_STYLE | wxCLIP_CHILDREN | wxRESIZE_BORDER)
+        : m_orientRadio((orientation == wxOrientation::wxHORIZONTAL) ? 0 : 1)
+        {
+        SetExtraStyle(GetExtraStyle() | wxWS_EX_BLOCK_EVENTS | wxWS_EX_CONTEXTHELP);
+        Wisteria::UI::DialogWithHelp::Create(parent, id, caption, pos, size, style);
+
+        CreateControls(imgPaths);
+        Centre();
+        }
+
+    /// @private
+    ImageMergeDlg(const ImageMergeDlg& that) = delete;
+    /// @private
+    ImageMergeDlg& operator=(const ImageMergeDlg& that) = delete;
+
+    [[nodiscard]]
+    const wxString& GetMergedFilePath() const noexcept
+        {
+        return m_mergedFilePath;
+        }
+
+  private:
+    void CreateControls(const wxArrayString& imgPaths);
+
+    void AdjustThumbnailsHorizontally();
+    void AdjustThumbnailsVertically();
+
+    int m_orientRadio{ 0 };
+    wxStaticBoxSizer* m_horizontalThumbsSizer{ nullptr };
+    wxStaticBoxSizer* m_verticalThumbsSizer{ nullptr };
+
+    wxString m_mergedFilePath;
+    };
+
+    /** @}*/
+
+#endif //__IMAGEMERGE_DLG_H__
