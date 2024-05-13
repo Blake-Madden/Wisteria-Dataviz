62c62773a778281f1336ab48232e6e0b06b9d01b
Blake-Madden <66873089+Blake-Madden@users.noreply.github.com>
2024-05-13 19:46:27 -0400

    Don't use direct write to buffer anymore

--- src/util/textstream.cpp
+++ src/util/textstream.cpp
@@ -111,8 +111,9 @@ namespace Wisteria
         // if 16-bit Unicode
         if (convertUnicodeText.is_unicode(text))
             {
-            convertUnicodeText.set_writable_buffer(dest, destLength);
             convertUnicodeText(text, length, wxIsPlatformLittleEndian());
+            wcsncpy_s(dest, destLength, convertUnicodeText.get_filtered_text(),
+                      convertUnicodeText.get_filtered_text_length());
             return true; // already null terminated, so we're done, return from here.
             }
         // if UTF-8 (or simply 7-bit ANSI)
