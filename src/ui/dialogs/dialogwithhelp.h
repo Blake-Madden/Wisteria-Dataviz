fd530281a1c266ea76435bb6f5668048d96bead4
Blake-Madden <66873089+Blake-Madden@users.noreply.github.com>
2025-09-15 21:25:26 -0400

    Add helper function to workaround bitmap size issued with bitmap controls under GTK

--- src/ui/dialogs/dialogwithhelp.h
+++ src/ui/dialogs/dialogwithhelp.h
@@ -12,6 +12,7 @@
 #ifndef WISTERIA_DIALOG_WITH_HELP_H
 #define WISTERIA_DIALOG_WITH_HELP_H
 
+#include "../../math/mathematics.h"
 #include <wx/dialog.h>
 #include <wx/filename.h>
 #include <wx/wx.h>
@@ -67,6 +68,52 @@ namespace Wisteria::UI
             {
             m_helpProjectFolder = helpProjectDirectory;
             m_helpTopic = topicPath;
+            }
+
+        /** @brief Fixes a wxBitmap from a wxBitmapBundle for use in wxGenericStaticBitmap on GTK.
+
+            @details On wxGTK, wxGenericStaticBitmap has a quirk where power-of-2 bitmap sizes
+            (e.g., 16x16, 32x32, 128x128) can be interpreted as stock icon sizes, causing
+            them to be drawn at 16x16 regardless of the actual bitmap size.
+
+            This function applies a 1-pixel downscale to power-of-2 sizes on GTK to bypass the
+            stock icon shortcut. Non-power-of-2 sizes, and all sizes on non-GTK platforms,
+            are returned unmodified.
+
+            @param bundle The @c  wxBitmapBundle containing the bitmap(s) to display.
+            @param size The intended display size of the bitmap. Must be fully specified
+                        (width and height > 0).
+            @return A wxBitmap suitable for use in @c wxGenericStaticBitmap.
+                    Returns @c wxNullBitmap if size is invalid.
+        */
+        static wxBitmap FixStaticBitmapImage(const wxBitmapBundle& bundle, wxSize size)
+            {
+            wxASSERT_MSG(size.GetWidth() > 0 && size.GetHeight() > 0,
+                         "FixStaticBitmapImage requires positive, fully-specified size!");
+
+            if (size.GetWidth() <= 0 || size.GetHeight() <= 0)
+                {
+                wxLogError("FixStaticBitmapImage called with invalid size: %dx%d", size.GetWidth(),
+                           size.GetHeight());
+                return wxNullBitmap;
+                }
+
+#ifdef __WXGTK__
+            if (!is_power_of_two(size.GetWidth()) || !is_power_of_two(size.GetHeight()))
+                {
+                // non-power-of-2 sizes are safe
+                return bundle.GetBitmap(size);
+                }
+
+            // power-of-2 size: downscale by 1 pixel to avoid GTK 16x16 quirk
+            wxBitmap bmp = bundle.GetBitmap(size);
+            wxBitmap::Rescale(bmp, wxSize{ size.GetWidth() - 1, size.GetHeight() - 1 });
+
+            return bmp;
+#else
+            // non-GTK: just return the requested size
+            return bundle.GetBitmap(size);
+#endif
             }
 
       private:
