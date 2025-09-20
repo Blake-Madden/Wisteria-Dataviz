71565494702c999b14852f5a3ef037834a6ff40a
Blake-Madden <66873089+Blake-Madden@users.noreply.github.com>
2025-09-20 06:36:26 -0400

    Ignore Catch2 build folder when GUI test runner looks for the real Catch2 folder

--- cmake/folders.cmake
+++ cmake/folders.cmake
@@ -16,3 +16,22 @@ function(find_directory DIR_TO_SEARCH_FOR FOUND_DIR)
     set(${FOUND_DIR} "")
     return(PROPAGATE ${FOUND_DIR})
 endfunction()
+
+# Looks for a folder in the project's folder that contains a CMakeLists.txt,
+# and if not found begins looking for it in parent folders.
+# First argument is the folder to look for, and the second is the result folder.
+function(find_cmake_project_directory DIR_TO_SEARCH_FOR FOUND_DIR)
+    set(CURRENT_SEARCH_DIR ${CMAKE_CURRENT_SOURCE_DIR})
+    foreach(i RANGE 1 5)
+        if(EXISTS "${CURRENT_SEARCH_DIR}/${DIR_TO_SEARCH_FOR}/CMakeLists.txt")
+            set(${FOUND_DIR} "${CURRENT_SEARCH_DIR}/${DIR_TO_SEARCH_FOR}")
+            return(PROPAGATE ${FOUND_DIR})
+        endif()
+        # look in the parent dir in the next loop
+        cmake_path(GET CURRENT_SEARCH_DIR PARENT_PATH CURRENT_SEARCH_DIR)
+    endforeach()
+
+    # couldn't find it
+    set(${FOUND_DIR} "")
+    return(PROPAGATE ${FOUND_DIR})
+endfunction()
