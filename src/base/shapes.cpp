e841b721ccfaaa141d660d36a9e1fde8506c031c
Blake-Madden <66873089+Blake-Madden@users.noreply.github.com>
2026-01-07 18:07:06 -0500

    Improve dollar shape

--- src/base/shapes.cpp
+++ src/base/shapes.cpp
@@ -3020,9 +3020,9 @@ namespace Wisteria::GraphItems
                 }
 
             // face
-            gc->SetPen((GetGraphItemInfo().GetBrush().GetColour().GetAlpha() < wxALPHA_OPAQUE) ?
-                           Colors::ColorContrast::ShadeOrTint(wxColour{ L"#ADADAD" }) :
-                           wxColour{ L"#3E3E3C" });
+            gc->SetPen(
+                wxPenInfo{ Colors::ColorContrast::ShadeOrTint(wxColour{ L"#ADADAD" }) }.Width(
+                    std::max<int>(1, ScaleToScreenAndCanvas(math_constants::tenth))));
             gc->SetBrush(Colors::ColorContrast::Tint(wxColour{ L"#BCC5D3" }));
             gc->DrawEllipse(faceRect);
 
