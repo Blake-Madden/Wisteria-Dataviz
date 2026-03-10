3c2ec850ed1abbc916e02dfdd67a60c26f325c2e
Blake-Madden <66873089+Blake-Madden@users.noreply.github.com>
2026-03-10 17:01:52 -0400

    Add missing include, fix icons

--- src/ui/app.cpp
+++ src/ui/app.cpp
@@ -14,6 +14,7 @@
 #include "wx/xrc/xmlres.h"
 #include <wx/regex.h>
 #include <wx/stc/stc.h>
+#include <wx/webrequest.h>
 #include <wx/xml/xml.h>
 
 /// @brief Temporarily turn off AppName being appended to @c wxStandardPaths calls.
